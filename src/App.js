dbace31c5f415eff37c88ca3176d713403c875b5
mrmitcp1 <mrmitcp55@gmail.com>
2023-07-06 10:09:25 +0700

    alert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import AlertComponent from "./component/AlertComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="alert alert-warning" role="alert">
+        <AlertComponent showAlert={  'Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại.'}/>
+      </div>
     </div>
   );
 }
---
+++ src/component/AlertComponent.js
@@ -0,0 +1,6 @@
+function AlertComponent(props) {
+    return(
+          <h1>{props.showAlert}</h1>
+    )
+}
+export default AlertComponent
